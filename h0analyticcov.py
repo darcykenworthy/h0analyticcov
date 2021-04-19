New code for handling duplicates Bugfix: wasn’t actually reweighting MU New scipy syntax for units funfunfun Fixed names for things

h0analyticcov.py
@@ -15,20 +15,7 @@ from math import ceil
 import re,argparse,os
  
 
-sdss=['16314','16392','16333','14318','17186','17784','7876']
-trunames=['2006oa','2006ob','2006on','2006py','2007hx','2007jg','2005ir']
-
-def readFitres(fileName):
-	"""Reads a fitres file into a structured np array"""
-	with open(fileName,'r') as file : fileText=file.read()
-	result=re.compile('VARNAMES:([\w\s]+)\n').search(fileText)
-	names= ['VARNAMES:']+[x for x in result.groups()[0].split() if not x=='']
-	namesToTypes={'VARNAMES':'U3','CID':'U20','FIELD':'U4','IDSURVEY':int}
-	types=[namesToTypes[x] if x in namesToTypes else float for x in names]
-	data=np.genfromtxt(fileName,skip_header=fileText[:result.start()].count('\n')+1,dtype=list(zip(names,types)))
-	for sdssName,truName in zip(sdss,trunames):
-			data['CID'][data['CID']==sdssName]=truName
-	return data
+from utilfunctions import *
 
 def calc_pk_nl_dlog10k(kmin,kspacing,kmax,sig_rsd,background=0):
 	"""Calculate the nonlinear power spectrum in log k with CLASS/halofit for a given minimum k, # of k points, and maximum k, along with a rsd term"""
@@ -123,23 +110,9 @@ if args.outputstem: outputstem+='-'+args.outputstem
 ##########################################################################################    
 
 sndata=readFitres(fitresfile)#sndata=readFitres('../../voidtest/SALT2mu_wcsphst2.fitres')
+sndata=renameDups(sndata)
 if args.cutdups:
-	# Eliminate duplicates
-	finalIndices=[]
-	for name in np.unique(sndata['CID']):
-		dups=sndata[sndata['CID']==name]
-		inds=np.where(sndata['CID']==name)[0]
-		#Prefer non-SDSS surveys
-		if (dups['IDSURVEY']==1).sum()>0 and (dups['IDSURVEY']!=1).sum()>0:
-			cut=dups['IDSURVEY']!=1
-			inds=inds[cut][::-1]
-			dups=dups[cut][::-1]
-		try:
-			finalIndices+=[inds[0]]
-		except:
-			print(name,sndata[sndata['CID']==name]['IDSURVEY'])
-			raise ValueError
-	sndata=sndata[finalIndices].copy()
+	sndata=cutdups(sndata)
 
 # r16names=np.genfromtxt('../voidtest/R16_hubflo.txt',skip_header=3,usecols=0,dtype='U20')
 # r16err=np.genfromtxt('../voidtest/R16_hubflo.txt',skip_header=3,usecols=2)
@@ -148,7 +121,28 @@ cut=( sndata[redshiftkey]>minred) & ( sndata[redshiftkey]<maxred)
 #cut= sndata['zCMB']<.5
 sndata=sndata[cut].copy()
 print(f'Calculating covariance matrix for {sndata.size} SNe')
- 
+if (sndata['DEC']<-90).any():
+	sndata[np.where(sndata['CID']=='SNF20080514-002')[0][0]]['RA']=202.303420
+	sndata[np.where(sndata['CID']=='SNF20080514-002')[0][0]]['DEC']=11.272390
+
+	sndata[np.where(sndata['CID']=='SNF20080514-002')[0][0]]['HOST_RA']=202.306840
+	sndata[np.where(sndata['CID']=='SNF20080514-002')[0][0]]['HOST_DEC']=11.275820
+
+
+	sndata[np.where(sndata['CID']=='SNF20080909-030')[0][0]]['RA']=330.454458
+	sndata[np.where(sndata['CID']=='SNF20080909-030')[0][0]]['DEC']= 13.055306
+
+	sndata[np.where(sndata['CID']=='SNF20080909-030')[0][0]]['HOST_RA']=330.456000
+	sndata[np.where(sndata['CID']=='SNF20080909-030')[0][0]]['HOST_DEC']= 13.055194
+
+	sndata[np.where(sndata['CID']=='SNF20071021-000')[0][0]]['RA']=3.749292
+	sndata[np.where(sndata['CID']=='SNF20071021-000')[0][0]]['DEC']=16.335000
+
+
+	sndata[np.where(sndata['CID']=='SNF20071021-000')[0][0]]['HOST_RA']=3.750292
+	sndata[np.where(sndata['CID']=='SNF20071021-000')[0][0]]['HOST_DEC']=16.333242
+	print('Warning: Special Case fixes for KAIT SNe')
+
 
 #Initialize CLASS
 classparams=classylss.load_ini(inifile)
@@ -254,7 +248,7 @@ velcovoutput='velocitycovariance-{}.npy'.format(outputstem)
 namelistoutput='snnames-{}.npy'.format(outputstem)
 distanceprefactor=D*f * (1+z)/ D_L * 5 /np.log(10)
 #Convert dDdtau from dimensionless to km/s
-velocityprefactor=dDdtau*constants.c/1e3
+velocityprefactor=dDdtau*constants.c.to(units.km/units.s).value
 if separatenonlinear:
 	velocitycovariance=np.outer(velocityprefactor,velocityprefactor)[np.newaxis,:,:]*integralterm
 else:

pecvelindividual.py
@@ -15,7 +15,7 @@ from astropy.coordinates import SkyCoord
 from astropy import units as u, constants
 from astropy.cosmology import FlatLambdaCDM,Planck15
 import pystan
-import re,timeit,pickle,argparse,multiprocessing,os
+import re,timeit,pickle,argparse,multiprocessing,os,sys
 import matplotlib.ticker as mtick
 from os import path
 import hashlib
@@ -59,29 +59,67 @@ parser.add_argument('--intrins',type=float,default=None,
                     help='Fix intrinsic scatter to a given value')
 
 args = parser.parse_args()
-
+print(' '.join(sys.argv))
 fitres=args.fitres
 niter,nchains=args.niter,args.nchains
 outputdir=args.outputdir
 redshiftcut= args.redshiftcut
-pickleoutput='{}.pickle'.format( path.splitext(path.split(fitres)[-1])[0])
 fixintrins=not (args.intrins is None)
 intrins=args.intrins
+dataname=path.splitext(path.split(fitres)[-1])[0]
+dataname+=f'{redshiftcut[0]:.2f}{redshiftcut[1]:.2f}'.replace('.','')
 
-if args.flatprior:
-	pickleoutput='flat_'+pickleoutput
+
+
+model_name=''
+if fixintrins:
+	model_name+='fixintrins_'
+if args.fixveldispersion:
+	model_name+='fixveldispersion_'
+if args.fixcorrectionparams:
+	model_name+='fixedcorrections_'
 if args.sampleprior:
-	pickleoutput='prior_'+pickleoutput
-	
+	model_name='prior_'+model_name
+else:
+	if args.nocorrections:
+		model_name='no2mm_'+model_name
+model_name+='individual'
+
+
+pickleoutput=f'{dataname}.pickle'
 os.makedirs(outputdir,exist_ok=True)
 pickleoutput=path.join(outputdir,pickleoutput)
+
+
 if path.exists(pickleoutput) and not args.clobber and not args.optimizing:
 	raise ValueError('Clobber is false and output file already exists')
 # In[3]:
 
     
 sndataFull=readFitres(fitres)
+sndataFull=renameDups(sndataFull)
 sndata=cutdups(sndataFull,reweight=True)
+if (sndata['DEC']<-90).any():
+	sndata[np.where(sndata['CID']=='SNF20080514-002')[0][0]]['RA']=202.303420
+	sndata[np.where(sndata['CID']=='SNF20080514-002')[0][0]]['DEC']=11.272390
+
+	sndata[np.where(sndata['CID']=='SNF20080514-002')[0][0]]['HOST_RA']=202.306840
+	sndata[np.where(sndata['CID']=='SNF20080514-002')[0][0]]['HOST_DEC']=11.275820
+
+
+	sndata[np.where(sndata['CID']=='SNF20080909-030')[0][0]]['RA']=330.454458
+	sndata[np.where(sndata['CID']=='SNF20080909-030')[0][0]]['DEC']= 13.055306
+
+	sndata[np.where(sndata['CID']=='SNF20080909-030')[0][0]]['HOST_RA']=330.456000
+	sndata[np.where(sndata['CID']=='SNF20080909-030')[0][0]]['HOST_DEC']= 13.055194
+
+	sndata[np.where(sndata['CID']=='SNF20071021-000')[0][0]]['RA']=3.749292
+	sndata[np.where(sndata['CID']=='SNF20071021-000')[0][0]]['DEC']=16.335000
+
+
+	sndata[np.where(sndata['CID']=='SNF20071021-000')[0][0]]['HOST_RA']=3.750292
+	sndata[np.where(sndata['CID']=='SNF20071021-000')[0][0]]['HOST_DEC']=16.333242
+	print('Warning: Special Case fixes for KAIT SNe')
 
 z=sndata['zCMB']
 
@@ -90,7 +128,7 @@ print(f'{cut.sum()} SNe in redshift range {redshiftcut}')
 sndata=sndata[cut].copy()
 z=sndata['zCMB']
 
-sncoords,separation,angsep=getseparation(sndata)
+sncoords,separation,angsep=getseparation(sndata,hostlocs=False)
 sndata=separatevpeccontributions(sndata,sncoords)
 
 try:
@@ -129,12 +167,10 @@ define_additional_data=''
 define_additional_constants=''
 define_additional_params=''
 define_additional_model_quantities=''
-model_name=''
 # In[13]:
 
 
 if fixintrins:
-	model_name+='fixintrins_'
 	define_additional_constants+=f"""
     real<lower=.01,upper=1> intrins={intrins};
     vector[N] sigmamuresids = sqrt(biasscale*square(intrins)+square(muerr));
@@ -148,7 +184,6 @@ else:
 """
 
 if args.fixveldispersion:
-	model_name+='fixveldispersion_'
 	define_additional_constants+="""
     real<lower=10,upper=500> veldispersion_additional=200;
     matrix[N,N] pecvelcovtot=pecvelcov+diag_matrix(rep_vector(square(veldispersion_additional),N));
@@ -161,7 +196,6 @@ else:
 	matrix[N,N] pecvelcovtot=pecvelcov+diag_matrix(rep_vector(square(veldispersion_additional),N));
 """
 if args.fixcorrectionparams:
-	model_name+='fixedcorrections_'
 	define_additional_constants+="""
 	real<lower=10,upper=1000> correctionstd=150;
 """
@@ -175,8 +209,6 @@ else:
 
 if args.flatprior:
 	prior_block="""
-	//lambdacdm prior
-	peculiarvelocities ~ multi_normal(zeros,pecvelcovtot);	
 """
 else:
 	prior_block="""
@@ -200,7 +232,6 @@ prior_block+="""
 	peculiarvelocities ~ multi_normal(zeros,pecvelcovtot);
 """
 if args.sampleprior:
-	model_name='prior_'+model_name
 	calculate_residuals="" 
 else:
 	if args.nocorrections:
@@ -211,7 +242,7 @@ else:
 		calculate_residuals="""
     //2M++ constraints
     vextrescale ~ normal(1,vextfracerr);
-    peculiarvelocities[correctedinds] ~ normal(velcorrections+bulkcorrections[correctedinds],correctionstd);
+    peculiarvelocities[correctedinds] ~ normal(velcorrections+bulkcorrections,correctionstd);
 """ 
 
 	calculate_residuals+="""
@@ -219,7 +250,6 @@ else:
 	muresiduals ~ normal(offset+dmudvpec .* peculiarvelocities, sigmamuresids);
 """
 
-model_name+='individual_model'
 model_code=f"""
 
 data {{
@@ -234,7 +264,7 @@ data {{
     int<lower=0> M; // number of 2M++ local corrections
     int  correctedinds[M]; //indices of SNe with local 2m++ corrections
     vector[M] velcorrections;
-    vector[N] bulkcorrections;
+    vector[M] bulkcorrections;
     matrix[N,N] pecvelcov;
     {define_additional_data}
     
@@ -260,7 +290,7 @@ parameters {{
 }}
 
 transformed parameters {{
-    real vextrescale = dot_product(bulkcorrections,peculiarvelocities)/bulkfiducial ;
+    real vextrescale = dot_product(bulkcorrections,peculiarvelocities[correctedinds])/bulkfiducial ;
 }}
 
 
@@ -281,7 +311,7 @@ else:
 	existingmodelisgood=False
 with open(codefile,'w') as file: file.write(model_code)
 
-hascorrection=sndata['VPEC_LOCAL']!=0
+hascorrection= sndata['zCMB']<0.06
 correctedindices=np.where(hascorrection)[0]
 
 standat = {'N': sndata.size,
@@ -295,7 +325,7 @@ standat = {'N': sndata.size,
                
                'correctedinds': correctedindices+1, #Stan indexes from 1 &*#$*@#^$
                'velcorrections': sndata['VPEC_LOCAL'][correctedindices],
-               'bulkcorrections': sndata['VPEC_BULK'],
+               'bulkcorrections': sndata['VPEC_BULK'][correctedindices],
                
                'pecvelcov':pecvelcov,
               }

utilfunctions.py
@@ -8,17 +8,18 @@ from itertools import combinations
 from astropy.coordinates import SkyCoord
 from astropy import units as u, constants
 from astropy.cosmology import FlatLambdaCDM,Planck15
-import pystan
 import re,timeit,pickle,argparse,multiprocessing,os
 import matplotlib.ticker as mtick
 from os import path
 import hashlib
 import csv
+from v3_0_duplicate_map import dups as dillon_dups
+dillon_dups=dillon_dups[3]
+
+
 cosmo=Planck15
 
 
-sdss=['16314','16392','16333','14318','17186','17784','7876']
-trunames=['2006oa','2006ob','2006on','2006py','2007hx','2007jg','2005ir']
 def readFitres(fileName):			
     with open(fileName,'r') as file : fileText=file.read()
     result=re.compile('VARNAMES:([\w\s]+)\n').search(fileText)
@@ -26,16 +27,15 @@ def readFitres(fileName):
     namesToTypes={'VARNAMES':'U3','CID':'U20','FIELD':'U4','IDSURVEY':int}
     types=[namesToTypes[x] if x in namesToTypes else float for x in names]
     data=np.genfromtxt(fileName,skip_header=fileText[:result.start()].count('\n')+1,dtype=list(zip(names,types)))
-    for sdssName,truName in zip(sdss,trunames):
-            data['CID'][data['CID']==sdssName]=truName
     return data
 
-def weightedMean(vals,covs,cut=None):
+def weightedMean(vals,covs,cut=None,returnpulls=False):
     if cut is None:cut=np.ones(vals.size,dtype=bool)
     if covs.ndim==1:
         vars=covs
         mean=((vals)/vars)[cut].sum()/(1/vars)[cut].sum()
         chiSquared=((vals-mean)**2/vars)[cut].sum()
+        pulls=((vals-mean)/np.sqrt(vars))[cut]
         var=1/((1/vars)[cut].sum())
     else:
         vals=vals[cut]
@@ -47,7 +47,10 @@ def weightedMean(vals,covs,cut=None):
         mean=var*np.dot(design,linalg.solve_triangular(transform,vals,lower=True))
         pulls=linalg.solve_triangular(transform,vals-mean,lower=True)
         chiSquared=np.dot(pulls,pulls)
-    return mean,np.sqrt(var),chiSquared
+    if returnpulls:
+    	return mean,np.sqrt(var),chiSquared,pulls
+    else:
+    	return mean,np.sqrt(var),chiSquared
 
 def addredshiftcolumnfromfile(redshiftfile,sndataFull,redshiftcolumn):
 	if redshiftfile.lower().endswith('.fitres'):
@@ -107,12 +110,40 @@ def writefitres(fitres,data):
 		for row in data:
 			writer.writerow(['SN:']+list(row)[1:])
 
+def renameDups(sndataFull):
+	sndataFull=sndataFull.copy()
+	uniqueids={}
+	coveredcids=set()
+	cids=np.unique(sndataFull['CID'])
+	for cid in (cids):
+		if cid in coveredcids: continue
+		#if cid == '10805': import pdb;pdb.set_trace()
+		coveredcids.add(cid)
+		name,aliases=cid,{cid}
+		while True:
+			for edge in dillon_dups.items():
+				if  (edge[0] in cids) and (edge[1] in cids) and (edge[0] in aliases)^(edge[1] in aliases):
+					aliases.add(edge[0])
+					aliases.add(edge[1])
+					coveredcids.add(edge[0])
+					coveredcids.add(edge[1])
+					break
+			else:
+				break
+		uniqueids[name]=aliases
+
+	for unique in uniqueids:
+		for alias in uniqueids[unique]:
+			sndataFull['CID'][sndataFull['CID']==alias]=unique
+	return sndataFull
 	
-def cutdups(sndataFull,reweight=True):
+def cutdups(sndataFull,reweight=False):
 	accum=[]
 	result=[]
 	finalInds=[]
 	sndataFull=sndataFull.copy()
+	
+	results=[]
 	for name in np.unique(sndataFull['CID']):
 		dups=sndataFull[sndataFull['CID']==name]
 		inds=np.where(sndataFull['CID']==name)[0]
@@ -123,10 +154,11 @@ def cutdups(sndataFull,reweight=True):
 		inds=sorted(inds,key=sortingkey)
 		finalInds+=[inds[0]]
 		if len(inds)>1 and reweight:
-			for x in ['MUERR_RAW','cERR','x1ERR']:
-				sndataFull[x][inds[0]],sndataFull[x][inds[0]],_=weightedMean(sndataFull[x],sndataFull[x]**2,sndataFull['CID']==name)
-
-	return sndataFull[finalInds].copy()
+			sndataFull['MU'][inds[0]],sndataFull['MUERR_RAW'][inds[0]],chisq,resids=weightedMean(sndataFull['MU'],sndataFull['MUERR_RAW']**2,sndataFull['CID']==name,returnpulls=True)
+			results+=[(name,chisq, (sndataFull['CID']==name).sum(),sndataFull[sndataFull['CID']==name]['IDSURVEY'],resids)]
+	return sndataFull[finalInds].copy()#,results
+	
+	
 def checkposdef(matrix,condition=1e-10):
 	vals,vecs=linalg.eigh(matrix)
 	if (vals>=vals.max()*condition).all(): return matrix
@@ -151,12 +183,13 @@ def separatevpeccontributions(sndata,sncoords):
 	sndata=np.array(recfunctions.append_fields(sndata,'VPEC_BULK',vpecbulk))
 	return sndata
 
-def getseparation(sndata):
+def getseparation(sndata,hostlocs=True):
 	zcmb=sndata['zCMB']
+	rakey='HOST_RA' if hostlocs else 'RA'
 	snra=sndata['RA']*u.degree
-	deckey='DEC' if 'DEC' in sndata.dtype.names else 'DECL'
+	deckey=('HOST_' if hostlocs else '')+'DEC' if 'DEC' in sndata.dtype.names else 'DECL'
 	sndec=sndata[deckey]*u.degree
-	sncoords=SkyCoord(ra=sndata['RA'],dec=sndata[deckey],unit=u.deg)
+	sncoords=SkyCoord(ra=snra,dec=sndec,unit=u.deg)
 
 	chi=cosmo.comoving_distance(zcmb).to(u.Mpc).value
 	snpos=np.zeros((sndata.size,3))