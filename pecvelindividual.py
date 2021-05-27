7b1b2c74c2cb02f5af423e9a909b60bb85e7930a
D'Arcy Kenworthy <accounts@darcykenworthy.com>
2021-05-27 10:41:34 -0400

    Analytically marginalized over latent peculiar velocities

--- pecvelindividual.py
+++ pecvelindividual.py
@@ -44,20 +44,23 @@ parser.add_argument('--njobs', type=int,default=0,
 
 parser.add_argument('--optimizing',action='store_true',
                     help='Useful for debugging: don\'t run mcmc, instead just return mode of posterior')
-parser.add_argument('--flatprior',action='store_true',
-                    help='Use flat priors on all parameters (default is Jeffreys prior)')
-parser.add_argument('--sampleprior', action='store_true', 
-                    help='Sample directly from prior distribution without constraints from data')
-
 parser.add_argument('--nocorrections',action='store_true',
 					help='No 2M++ corrections')
+
+					
+					
 parser.add_argument('--fixcorrectionparams',action='store_true',
 					help='Fix 2M++ nuisance parameters to fiducial values')
 parser.add_argument('--fixveldispersion',action='store_true',
 					help='Fix nonlinear velocity dispersion to 250 km/s')
 parser.add_argument('--intrins',type=float,default=None,
                     help='Fix intrinsic scatter to a given value')
-
+parser.add_argument('--flatprior',action='store_true',
+                    help='Use flat priors on all parameters (default is Jeffreys prior)')
+parser.add_argument('--sampleprior', action='store_true', 
+                    help='Sample directly from prior distribution without constraints from data')
+parser.add_argument('--generatedquantities',action='store_true',
+					help='Generate posterior predictive data and allow excluded data')
 args = parser.parse_args()
 print(' '.join(sys.argv))
 fitres=args.fitres
@@ -67,7 +70,7 @@ redshiftcut= args.redshiftcut
 fixintrins=not (args.intrins is None)
 intrins=args.intrins
 dataname=path.splitext(path.split(fitres)[-1])[0]
-dataname+=f'{redshiftcut[0]:.2f}{redshiftcut[1]:.2f}'.replace('.','')
+dataname+=f'_{redshiftcut[0]:.2f}_{redshiftcut[1]:.2f}'.replace('.','')
 
 
 
@@ -80,13 +83,10 @@ if args.fixcorrectionparams:
 	model_name+='fixedcorrections_'
 if args.sampleprior:
 	model_name='prior_'+model_name
-else:
-	if args.nocorrections:
-		model_name='no2mm_'+model_name
 model_name+='individual'
 
 
-pickleoutput=f'{dataname}.pickle'
+pickleoutput=f'{model_name}_{dataname}.pickle'
 os.makedirs(outputdir,exist_ok=True)
 pickleoutput=path.join(outputdir,pickleoutput)
 
@@ -163,35 +163,79 @@ nonlinear=checkposdef(nonlinear)
 
 
 
-define_additional_data=''
 define_additional_constants=''
 define_additional_params=''
-define_additional_model_quantities=''
+
 # In[13]:
-define_vpec_uncertainties="""
-	matrix[N,N] pecvelcovtot=pecvelcov;
+declare_pecvel_quantities="""
+	matrix[N,N] pecvelcovmarginal;
+	vector[N] pecvelmeanmarginal;
 """
-define_mu_uncertainties="""
-	vector[N] sigmamuresids = sqrt(square(veldispersion_additional*dmudvpec) + biasscale .* (square(intrins)+square(muerr)));
+define_pecvel_quantities="""
+	if (ntmppobs==0){
+		pecvelcovmarginal=pecvelcov;
+		pecvelmeanmarginal=rep_vector(0,N);
+	}
+	else if (ntmppobs==N){
+		matrix[N,N] sumcovtransform;
+		matrix[N,N] sumcovtransform_times_priorcov;
+		matrix[N,N] sumcovtransform_times_tmppcov;
+
+		sumcovtransform=cholesky_decompose(pecvelcov+diag_matrix(rep_vector(square(correctionstd),N)));
+		sumcovtransform_times_priorcov=  mdivide_left_tri_low(sumcovtransform,pecvelcov);
+		sumcovtransform_times_tmppcov=  mdivide_left_tri_low(sumcovtransform, diag_matrix(rep_vector(square(correctionstd),N)));
+		
+		pecvelmeanmarginal=sumcovtransform_times_priorcov'* mdivide_left_tri_low(sumcovtransform,velcorrections+bulkcorrections);
+		pecvelcovmarginal= sumcovtransform_times_tmppcov' *sumcovtransform_times_priorcov ;
+	}
+	else{
+		//quantities purely for calculation, no relevance
+		matrix[ntmppobs,ntmppobs] sumcovtransform;
+		matrix[ntmppobs,ntmppobs] sumcovtransform_times_priorcov;
+		matrix[ntmppobs,ntmppobs] sumcovtransform_times_tmppcov;
+		matrix[ntmppobs,N-ntmppobs] sumcovtransform_times_transfermatrix;
+
+		sumcovtransform=cholesky_decompose(pecvelcov[tmppinds,tmppinds]+diag_matrix(rep_vector(square(correctionstd),ntmppobs)));
+		sumcovtransform_times_priorcov=  mdivide_left_tri_low(sumcovtransform,pecvelcov[tmppinds,tmppinds]);
+		sumcovtransform_times_tmppcov=  mdivide_left_tri_low(sumcovtransform, diag_matrix(rep_vector(square(correctionstd),ntmppobs)));
+		sumcovtransform_times_transfermatrix=  mdivide_left_tri_low(sumcovtransform, pecvelcov[tmppinds,notmppinds] );
+
+		pecvelmeanmarginal[tmppinds]=sumcovtransform_times_priorcov'* mdivide_left_tri_low(sumcovtransform,velcorrections+bulkcorrections);
+		pecvelmeanmarginal[notmppinds]=rep_vector(0,N-ntmppobs);
+
+		pecvelcovmarginal[tmppinds,tmppinds]= sumcovtransform_times_tmppcov' *sumcovtransform_times_priorcov ;
+		pecvelcovmarginal[tmppinds,notmppinds]=  sumcovtransform_times_tmppcov' *sumcovtransform_times_transfermatrix ;
+		pecvelcovmarginal[notmppinds,tmppinds]= pecvelcovmarginal[tmppinds,notmppinds]';
+		pecvelcovmarginal[notmppinds,notmppinds]=pecvelcov[notmppinds,notmppinds]- crossprod(sumcovtransform_times_transfermatrix);
+	}
+"""
+
+declare_mu_quantities="""
+	vector[N] meanmuresids;
+	vector[N] muresidsvar ;
+	matrix[N,N] covsigmamuresids;
+"""
+define_mu_quantities="""
+	meanmuresids=offset+(dmudvpec .* pecvelmeanmarginal);
+	muresidsvar= (square(veldispersion_additional * dmudvpec) + biasscale .* (square(intrins)+square(muerr)));
+	covsigmamuresids=quad_form_diag(pecvelcovmarginal,dmudvpec)+diag_matrix(muresidsvar);
 """
 if fixintrins:
 	define_additional_constants+=f"""
-    real<lower=.01,upper=1> intrins={intrins};"""+define_mu_uncertainties
+    real<lower=.01,upper=1> intrins={intrins};"""
 else:
 	define_additional_params+="""
     real<lower=.01,upper=1> intrins;
 """
-	define_additional_model_quantities+=define_mu_uncertainties
 
 if args.fixveldispersion:
 	define_additional_constants+="""
     real<lower=10,upper=500> veldispersion_additional=200;
-"""+define_vpec_uncertainties
+"""
 else:
 	define_additional_params+="""
     real<lower=10,upper=500> veldispersion_additional;
 """
-	define_additional_model_quantities+=define_vpec_uncertainties
 if args.fixcorrectionparams:
 	define_additional_constants+="""
 	real<lower=10,upper=1000> correctionstd=150;
@@ -220,33 +264,19 @@ else:
 	target+=-log(veldispersion_additional);
 	target+=-log(intrins);
 	target+=-log(correctionstd);
-	
 """
-prior_block+="""
-	//lambdacdm prior
-	peculiarvelocities ~ multi_normal(zeros,pecvelcovtot);
-"""
+
+
 if args.sampleprior:
-	calculate_residuals="" 
+	calculate_residuals=""
 else:
-	if args.nocorrections:
-		calculate_residuals="""
-	//No 2M++ constraints
-	"""
-	else:
-		calculate_residuals="""
-    //2M++ constraints
-    peculiarvelocities[tmppinds]~ normal(velcorrections+bulkcorrections,correctionstd);
-""" 
 
-	calculate_residuals+="""
+	calculate_residuals="""
 	//SN constraints
-	muresiduals ~ normal(offset+(dmudvpec .* peculiarvelocities)[sninds], sigmamuresids[sninds]);
-
+	muresiduals ~ multi_normal(meanmuresids[sninds],covsigmamuresids[sninds,sninds] );
 """
 
 model_code=f"""
-
 data {{
     int<lower=0> N; // number of SNe
     vector<lower=0>[N] muerr; 
@@ -266,85 +296,124 @@ data {{
     int<lower=0> ntmppobs; // number of 2M++ local corrections
     int  tmppinds[ntmppobs]; //indices of SNe with local 2m++ corrections
     vector[ntmppobs] velcorrections;
-    vector[ntmppobs] bulkcorrections;
- 	
- 	int<lower=0> ntmppobs_pred;
-	int tmppinds_pred[ntmppobs_pred];
-	vector[ntmppobs_pred] tmpp_excluded;
-    {define_additional_data}
-    
+    vector[ntmppobs] bulkcorrections;    
 }}
 
 transformed data{{
     vector[N] zeros= rep_vector(0,N);
     cov_matrix[N] identity=diag_matrix(rep_vector(1.0,N));
 
-    real vextfracerr= 23./159;
-
-    real bulkfiducial=dot_self(bulkcorrections);
-    {define_additional_constants}
-
+    int notmppinds[N-ntmppobs];
+    int j=1;
+    int k=1;
+    for (i in 1:ntmppobs){{
+    	while (j<tmppinds[i]){{
+    		notmppinds[k]=j;
+    		k=k+1;
+    		j=j+1;
+    	}}
+    	j=j+1;
+    }}
+    while (j< N+1){{
+    	notmppinds[k]=j;
+    	k=k+1;
+    	j=j+1;
+    }}
+{define_additional_constants}
 }}
 
-
 parameters {{
-    real<offset=0,multiplier=.1> offset;
-    vector<offset=0,multiplier=400>[N] peculiarvelocities; 
-    
+    real<offset=0,multiplier=.1> offset;    
     {define_additional_params}
 }}
 
-transformed parameters {{
-    real vextrescale = dot_product(bulkcorrections,peculiarvelocities[tmppinds])/bulkfiducial ;
-}}
-
-
 model {{
-    
-    {define_additional_model_quantities}
+{declare_pecvel_quantities}
+{declare_mu_quantities}
 
-    {prior_block}
-    {calculate_residuals}
+{define_pecvel_quantities}
+{define_mu_quantities}
+   
+{prior_block}
+{calculate_residuals}
 }}
 
-generated quantities {{
-    // elementwise log likelihood
-    real log_likelihood_mu[nsnobs];
-    real log_likelihood_tmpp[ntmppobs];
-    
-    // posterior predictive
-    real mu_hat[nsnobs];
-    real tmpp_hat[ntmppobs];
-    
-    // out of sample prediction
-    real mu_pred[nsnobs_pred];
-    real tmpp_pred[ntmppobs_pred];
-    real log_likelihood_mu_pred[nsnobs_pred];
-    real log_likelihood_tmpp_pred[ntmppobs_pred];
-    
-    {define_additional_model_quantities}
-    // posterior predictive
-    for (k in 1:nsnobs){{
-		log_likelihood_mu[k] = normal_lpdf(muresiduals[k] | offset+(dmudvpec .* peculiarvelocities)[sninds[k]], sigmamuresids[sninds[k]]);
-    }}
-	mu_hat = normal_rng(offset+(dmudvpec .* peculiarvelocities)[sninds], sigmamuresids[sninds]);
-	
-    for (k in 1:ntmppobs){{
-		log_likelihood_tmpp[k] = normal_lpdf(velcorrections[k]+bulkcorrections[k] | peculiarvelocities[tmppinds[k]],correctionstd);
-	}}
-	tmpp_hat = normal_rng(peculiarvelocities[tmppinds],correctionstd);
- 
-    // out of sample prediction
-	mu_pred = normal_rng(offset+(dmudvpec .* peculiarvelocities)[sninds_pred], sigmamuresids[sninds_pred]);
-    for (k in 1:nsnobs_pred){{
-		log_likelihood_mu_pred[k] = normal_lpdf(mu_excluded[k]|offset+(dmudvpec .* peculiarvelocities)[sninds_pred[k]], sigmamuresids[sninds_pred[k]]);
-	}}
-	tmpp_pred = normal_rng(peculiarvelocities[tmppinds_pred],correctionstd);
-    for (k in 1:ntmppobs_pred){{
-		log_likelihood_tmpp_pred[k] = normal_lpdf(tmpp_excluded[k]|peculiarvelocities[tmppinds_pred[k]],correctionstd);
-	}}
-}}
 """
+if args.generatedquantities:
+	model_code+=f"""
+generated quantities{{
+	vector[nsnobs] mu_hat;
+	vector[nsnobs_pred] mu_pred;
+	real log_lik;
+	real log_lik_ex;
+	{{
+{declare_pecvel_quantities}
+{declare_mu_quantities}
+
+{define_pecvel_quantities}
+{define_mu_quantities}
+
+		log_lik = multi_normal_lpdf( muresiduals|meanmuresids[sninds], covsigmamuresids[sninds,sninds] );
+		mu_hat = multi_normal_rng(meanmuresids[sninds], covsigmamuresids[sninds,sninds] );
+		if (nsnobs_pred !=0){{
+			matrix[nsnobs,nsnobs] chol_sigma ;
+			vector[nsnobs] invchol_muobs;
+			matrix[nsnobs,nsnobs_pred] invchol_transfermatrix ;
+			vector[nsnobs_pred] meanpred;
+			matrix[nsnobs_pred,nsnobs_pred] sigmamupred;
+			chol_sigma = cholesky_decompose( covsigmamuresids[sninds,sninds]);
+			invchol_muobs= mdivide_left_tri_low(chol_sigma, muresiduals - (  meanmuresids[sninds]));
+			invchol_transfermatrix = mdivide_left_tri_low(chol_sigma,  covsigmamuresids[sninds,sninds_pred]);
+			
+			meanpred= meanmuresids[sninds_pred] + invchol_transfermatrix' * invchol_muobs;
+			sigmamupred=  covsigmamuresids[sninds_pred,sninds_pred]- crossprod(invchol_transfermatrix);
+		
+			log_lik_ex=multi_normal_lpdf(mu_excluded| meanpred,   sigmamupred);
+			mu_pred= multi_normal_rng(meanpred, sigmamupred);
+		}}
+	}}
+}}
+
+"""
+# generated quantities {{
+#     // elementwise log likelihood
+#     real log_likelihood_mu[nsnobs];
+#     real log_likelihood_tmpp[ntmppobs];
+#     
+#     // posterior predictive
+#     real mu_hat[nsnobs];
+#     real tmpp_hat[ntmppobs];
+#     
+#     // out of sample prediction
+#     real mu_pred[nsnobs_pred];
+#     real tmpp_pred[ntmppobs_pred];
+#     real log_likelihood_mu_pred[nsnobs_pred];
+#     real log_likelihood_tmpp_pred[ntmppobs_pred];
+#     {{
+# 		{define_additional_model_quantities}
+# 		// posterior predictive
+# 		for (k in 1:nsnobs){{
+# 			log_likelihood_mu[k] = normal_lpdf(muresiduals[k] | offset+(dmudvpec .* peculiarvelocities)[sninds[k]], sigmamuresids[sninds[k]]);
+# 		}}
+# 		mu_hat = normal_rng(offset+(dmudvpec .* peculiarvelocities)[sninds], sigmamuresids[sninds]);
+# 	
+# 		for (k in 1:ntmppobs){{
+# 			log_likelihood_tmpp[k] = normal_lpdf(velcorrections[k]+bulkcorrections[k] | peculiarvelocities[tmppinds[k]],correctionstd);
+# 		}}
+# 		tmpp_hat = normal_rng(peculiarvelocities[tmppinds],correctionstd);
+#  
+# 		// out of sample prediction
+# 		mu_pred = normal_rng(offset+(dmudvpec .* peculiarvelocities)[sninds_pred], sigmamuresids[sninds_pred]);
+# 		for (k in 1:nsnobs_pred){{
+# 			log_likelihood_mu_pred[k] = normal_lpdf(mu_excluded[k]|offset+(dmudvpec .* peculiarvelocities)[sninds_pred[k]], sigmamuresids[sninds_pred[k]]);
+# 		}}
+# 		tmpp_pred = normal_rng(peculiarvelocities[tmppinds_pred],correctionstd);
+# 		for (k in 1:ntmppobs_pred){{
+# 			log_likelihood_tmpp_pred[k] = normal_lpdf(tmpp_excluded[k]|peculiarvelocities[tmppinds_pred[k]],correctionstd);
+# 		}}
+# 	}}
+# }}
+
 codefile=path.join(outputdir,f'{model_name}.stan')
 if path.exists(codefile):
 	with open(codefile,'r') as file: oldcode=file.read()
@@ -353,8 +422,12 @@ else:
 	existingmodelisgood=False
 with open(codefile,'w') as file: file.write(model_code)
 
-hascorrection= sndata['zCMB']<0.06
-correctedindices=np.where(hascorrection)[0]
+if args.nocorrections:
+
+	hascorrection= sndata['zCMB']<0.06
+	correctedindices=np.where(hascorrection)[0]
+else:
+	correctedindices=np.array([],dtype=int)
 usesn=np.arange(sndata.size)
 standat = {'N': sndata.size,
                'muerr': sndata['MUERR_RAW'],
@@ -373,12 +446,12 @@ standat = {'N': sndata.size,
                'sninds':usesn+1,
                 'muresiduals': muresiduals[usesn],
                 
-               'nsnobs_pred':1,
-               'sninds_pred':np.array([1],dtype=int),
-               'mu_excluded':np.array([0],dtype=float),
-               'ntmppobs_pred':1,
-               'tmppinds_pred':np.array([1],dtype=int),
-               'tmpp_excluded':np.array([0],dtype=float),
+               'nsnobs_pred':0,
+               'sninds_pred':np.array([],dtype=int),
+               'mu_excluded':np.array([],dtype=float),
+               'ntmppobs_pred':0,
+               'tmppinds_pred':np.array([],dtype=int),
+               'tmpp_excluded':np.array([],dtype=float),
               }
 if fixintrins:
 	standat['intrins']=intrins
@@ -399,7 +472,6 @@ initfun=lambda :{
     'veldispersion_additional': np.exp(np.random.normal(np.log(200),.1)),
     'intrins': np.exp(np.random.normal(np.log(.12),.1)),
     'correctionstd': np.exp(np.random.normal(np.log(150),.1)),
-    'peculiarvelocities': np.random.multivariate_normal(sndata['VPEC_LOCAL']+sndata['VPEC_BULK'],pecvelcov+np.diag(200**2*np.ones(sndata.size)))
 }
 opfit = model.optimizing(data=standat,init=initfun,iter=niter)
 print(opfit)
